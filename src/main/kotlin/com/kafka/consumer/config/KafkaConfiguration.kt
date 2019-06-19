6f5f204318f1c15aaae74a40c677bbcc2b7ff82d
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2019-06-19 18:04:46 +0800

    Create listener for test_json topic

--- src/main/kotlin/com/kafka/consumer/config/KafkaConfiguration.kt
+++ src/main/kotlin/com/kafka/consumer/config/KafkaConfiguration.kt
@@ -1,5 +1,6 @@
 package com.kafka.consumer.config
 
+import com.kafka.consumer.model.User
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
@@ -11,12 +12,12 @@ import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.context.annotation.Bean
 import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.support.serializer.JsonDeserializer
 
 
 @EnableKafka
 @Configuration
 class KafkaConfiguration {
-
 
     @Bean
     fun consumerFactory(): ConsumerFactory<String, Any> {
@@ -36,5 +37,25 @@ class KafkaConfiguration {
         factory.setConsumerFactory(consumerFactory())
         return factory
     }
+
+    @Bean
+    fun userConsumerFactory(): ConsumerFactory<String, User> {
+        val config = mutableMapOf<String, Any >()
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "my-kafka:9092")
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_json")
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer::class.java)
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer::class.java)
+        return DefaultKafkaConsumerFactory<String, User>(config, StringDeserializer(),
+                JsonDeserializer(User::class.java))
+    }
+
+    @Bean
+    fun userKafkaListenerFactory(): ConcurrentKafkaListenerContainerFactory<String, User> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, User>()
+        factory.setConsumerFactory(userConsumerFactory())
+        return factory
+    }
+
 
 }
--- src/main/kotlin/com/kafka/consumer/listener/KafkaConsumer.kt
+++ src/main/kotlin/com/kafka/consumer/listener/KafkaConsumer.kt
@@ -8,10 +8,15 @@ import org.springframework.kafka.annotation.KafkaListener
 @Service
 class KafkaConsumer{
 
-    @KafkaListener(topics = ["test"], groupId = "group")
+    @KafkaListener(topics = ["test"], groupId = "group_id")
     fun consume(message: String) {
         println("Consumed message: $message")
     }
 
+    @KafkaListener(topics = ["test_json"], groupId = "group_json",
+            containerFactory = "userKafkaListenerFactory")
+    fun consumeJson(message: Any) {
+        println("Consumed message: $message")
+    }
 
 }
